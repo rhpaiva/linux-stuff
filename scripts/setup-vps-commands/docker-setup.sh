0e06e3af9776c8da28c84ca0458b35ee26fe1516
Rodrigo Paiva <rhpaiva@gmail.com>
2016-05-17 23:40:35 +0200

    Added test to docker setup

--- scripts/setup-vps-commands/docker-setup.sh
+++ scripts/setup-vps-commands/docker-setup.sh
@@ -16,7 +16,8 @@ apt-get update \
 # Add the new GPG key
 # add the repo to apt's sources
 # install docker and a syntax for vim
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D \
+test $? -eq 0 \
+&& apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D \
 && echo deb https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list \
 && apt-get update \
 && apt-get --assume-yes install docker-engine
